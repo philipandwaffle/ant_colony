dafc5388a40cbbf01a34b7cc51eb6267cf7ef0a8
philipandwaffle <philipcox197@gmail.com>
2023-02-16 18:41:25 +0000

    comments

--- src/resource.rs
+++ src/resource.rs
@@ -29,16 +29,20 @@ enum PileCount {
     Empty,
 }
 
+// contains the piles resource type and count
 #[derive(Component)]
 pub struct Pile {
     pile_type: PileType,
     pile_count: PileCount,
 }
 impl Pile {
+    // harvest pile, return true if it can be harvested
     fn harvest(&mut self, amount: u32) -> bool {
+        // get the resource count
         if let PileCount::HasResources(mut count) = self.pile_count {
             match count.checked_sub(amount) {
                 Some(new_count) => {
+                    // set new count
                     count = new_count;
                     return true;
                 }
@@ -51,9 +55,12 @@ impl Pile {
     }
 }
 
+// spawns piles that are added to the spawn list
 fn pile_spawner(mut commands: Commands, mut piles: ResMut<PileSpawnList>) {
     while !piles.to_spawn.is_empty() {
         let pile = piles.to_spawn.pop().unwrap();
+
+        // get the resource count
         if let PileCount::HasResources(count) = pile.pile_count {
             let radius = (count as f32).powf(1.0 / 3.0);
             let color = match pile.pile_type {
@@ -61,6 +68,7 @@ fn pile_spawner(mut commands: Commands, mut piles: ResMut<PileSpawnList>) {
                 PileType::Protein => Color::BLUE,
             };
 
+            // spawn the pile
             commands
                 .spawn(pile)
                 .insert(GeometryBuilder::build_as(
@@ -77,6 +85,7 @@ fn pile_spawner(mut commands: Commands, mut piles: ResMut<PileSpawnList>) {
         }
     }
 }
+// despawns entities whenever they run out
 fn pile_despawner(mut commands: Commands, piles: Query<(Entity, &Pile), Changed<Pile>>) {
     for (e, pile) in piles.iter() {
         if pile.pile_count == PileCount::Empty {
